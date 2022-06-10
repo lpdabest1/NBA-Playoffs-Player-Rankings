e58c8fd1aeb46cda52f3949e1c01edb968d09d44
lpdabest1 <lorenzo.pesina@utexas.edu>
2022-06-09 19:25:17 -0500

    google-cloud-included

--- App/pages/Home.py
+++ App/pages/Home.py
@@ -2,9 +2,32 @@ from operator import index
 import py_compile
 import streamlit as st 
 import pandas as pd
+from google.oauth2 import service_account
+from google.cloud import storage
 
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
 
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
 
+bucket_name = "nba-playoff-player-data"
+file_path = "nba_playoff_players.csv"
+
+content = read_file(bucket_name, file_path)
+
+# Print results.
+for line in content.strip().split("\n"):
+    name, pet = line.split(",")
+    st.write(f"{name} has a :{pet}:")
 
 
 st.markdown(
@@ -17,6 +40,7 @@ This Application focuses on comparing the best playoff performances in the NBA o
 
 nba_data = pd.read_csv('App/data-scraper/nba_playoff_players.csv')
 nba_data_test = pd.DataFrame(nba_data)
+st.write("non-google dataframe")
 st.dataframe(nba_data)
 
 hide_dataframe_row_index = """
@@ -27,5 +51,5 @@ hide_dataframe_row_index = """
             """
 
 
-st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-st.dataframe(nba_data)
+#st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+#st.dataframe(nba_data)
